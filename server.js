9de91861acc6a1e3c6c0e35a1ee82713e458b321
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-04-17 00:03:33 +0300

    Swagger UI

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
+// API DOcumenATion
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
 // packages imports
-import express from 'express';
-import 'express-async-errors';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
 //securty packges
-import helmet from 'helmet';
-import xss from 'xss-clean';
-import mongoSanitize from 'express-mongo-sanitize';
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 // files imports
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
 // routes import
-import testRoutes from './routes/testRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import errroMiddelware from './middelwares/errroMiddleware.js';
-import jobsRoutes from './routes/jobsRoute.js';
-import userRoutes from './routes/userRoutes.js';
+import testRoutes from "./routes/testRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import errroMiddelware from "./middelwares/errroMiddleware.js";
+import jobsRoutes from "./routes/jobsRoute.js";
+import userRoutes from "./routes/userRoutes.js";
 
 //Dot ENV config
 dotenv.config();
 
 // mongodb connection
 connectDB();
+
+// Swagger api config
+// swagger api options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Expressjs Job Portal Application",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+        //         url: "https://nodejs-job-portal-app.onrender.com"
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
 
 //rest object
 const app = express();
@@ -33,13 +57,16 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //routes
-app.use('/api/v1/test', testRoutes);
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/job', jobsRoutes);
+app.use("/api/v1/test", testRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/job", jobsRoutes);
+
+//homeroute root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 
 //validation middelware
 app.use(errroMiddelware);
@@ -48,5 +75,8 @@ app.use(errroMiddelware);
 const PORT = process.env.PORT || 8080;
 //listen
 app.listen(PORT, () => {
-  console.log(`Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`.bgCyan.white);
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} Mode on port no ${PORT}`
+      .bgCyan.white
+  );
 });
